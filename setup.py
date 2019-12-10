026c77dd7201c5ae423ddc7398c11eb5c33ed26f
Cameron Voloshin <clvoloshin@Camerons-MacBook-Pro.local>
2019-12-10 12:12:45 -0800

    update

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(name='ope',
+setup(name='OPETools',
       version='0.1',
       description='Off Policy Policy Evaluation Toolkit',
-      url='http://github.com/clvoloshin/ope',
+      url='https://github.com/clvoloshin/OPE-tools',
       author='Cameron Voloshin',
       author_email='clvoloshin@gmail.com',
       license='MIT',
